d959c0db90df04f023cb9575c69d0cd2ff24d1f8
Mariya Gedrich <maryged@gmail.com>
2015-02-08 23:26:17 -0500

    folder

---
+++ 01-recursion/Recursion.java
@@ -0,0 +1,93 @@
+public class Recursion {
+    public int factorial(int n) {
+	if (n==0) {
+	    return 1;
+	} else {
+	    return n*factorial(n-1);
+	}
+    }
+
+    public int fibonacci(int n) {
+	if (n==0) {
+	    return 0;
+	}
+	else if (n<3) {
+	    return 1;
+	}
+	else {
+	    return (fibonacci(n-1)+fibonacci(n-2));
+	}
+    }
+
+    public int len(String s) {
+	if (s.equals("")) {
+	    return 0;
+	} else {
+	    return 1+len(s.substring(1));
+	}
+    }
+
+    public int count(String s, char c) {
+	if (s.equals("")) {
+	    return 0;
+	} else {
+	    if (s.charAt(0)==c) {
+		return 1+count(s.substring(1),c);
+	    } else {
+		return count(s.substring(1),c);
+	    }
+	}
+    }
+
+    public int bunnyEars2(int bunnies) {
+	if (bunnies==0) {
+	    return 0;
+	} else {
+	    if (bunnies%2==0) {
+		return 3+bunnyEars2(bunnies-1);
+	    }
+	    else {
+		return 2+bunnyEars2(bunnies-1);
+	    }
+	}
+    }
+
+    public int strCount(String str, String sub) {
+	if (str.length()<sub.length()) {
+	    return 0;
+	} else 
+	    if ((str.substring(0,sub.length())).equals(sub)){
+		return 1+strCount(str.substring(sub.length()),sub);
+	    }  else {
+		return strCount(str.substring(1),sub);
+	    }
+    }
+
+    public int sumDigits(int n) {
+	if (n<10) {
+	    return n;
+	} else {
+	    return (n%10)+sumDigits(n/10);
+	}
+    }
+
+    public String allStar(String str) {
+	if (str.equals("")||str.length()==1) {
+	    return str;
+	} else {
+	    return str.charAt(0)+"*"+allStar(str.substring(1));
+	}
+    }
+
+    public static void main(String[] args) {
+	Recursion r=new Recursion();
+	System.out.println(r.factorial(3));
+	System.out.println(r.len("one"));
+	System.out.println(r.count("allStar",'l'));
+    }
+    
+
+
+    
+
+}
