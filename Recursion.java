d3761b68c400e18bd8e89484b53538758bcda8c0
Mariya Gedrich <maryged@gmail.com>
2015-02-08 23:21:29 -0500

    homework1

--- Recursion.java
+++ Recursion.java
@@ -1,4 +1,12 @@
 public class Recursion {
+    public int factorial(int n) {
+	if (n==0) {
+	    return 1;
+	} else {
+	    return n*factorial(n-1);
+	}
+    }
+
     public int fibonacci(int n) {
 	if (n==0) {
 	    return 0;
@@ -8,6 +16,26 @@ public class Recursion {
 	}
 	else {
 	    return (fibonacci(n-1)+fibonacci(n-2));
+	}
+    }
+
+    public int len(String s) {
+	if (s.equals("")) {
+	    return 0;
+	} else {
+	    return 1+len(s.substring(1));
+	}
+    }
+
+    public int count(String s, char c) {
+	if (s.equals("")) {
+	    return 0;
+	} else {
+	    if (s.charAt(0)==c) {
+		return 1+count(s.substring(1),c);
+	    } else {
+		return count(s.substring(1),c);
+	    }
 	}
     }
 
@@ -51,8 +79,12 @@ public class Recursion {
 	}
     }
 
-
-    
+    public static void main(String[] args) {
+	Recursion r=new Recursion();
+	System.out.println(r.factorial(3));
+	System.out.println(r.len("one"));
+	System.out.println(r.count("allStar",'l'));
+    }
     
 
 
