10e987fc3e91d1db94b40c554fd91f4fc59b84c1
Mariya Gedrich <maryged@gmail.com>
2015-02-26 00:05:47 -0500

    I eMERGE from the rubble with a working program

--- 04-sorting/Merj.java
+++ 04-sorting/Merj.java
@@ -1,8 +1,17 @@
+import java.util.*;
+import java.io.*;
+
 public class Merj {
     public ArrayList<Integer> merge(ArrayList<Integer> a, ArrayList<Integer> b) {
 	ArrayList<Integer> r= new ArrayList<Integer>();
 	while (a.size()+b.size()>0) {
-	    if (a.get(0)>b.get(0)) {
+	    if (a.size()==0) {
+		r.addAll(b);
+		b.clear();
+	    } else if (b.size()==0) {
+		r.addAll(a);
+		a.clear();
+	    } else if (a.get(0)>b.get(0)) {
 		r.add(b.remove(0));
 	    } else {
 		r.add(a.remove(0));
@@ -15,7 +24,7 @@ public class Merj {
 	Merj m=new Merj();
 	ArrayList<Integer> a=new ArrayList<Integer>();
 	ArrayList<Integer> b=new ArrayList<Integer>();
-	for (int i=0;i<5;i++) {
+	for (int i=0;i<13;i++) {
 	    a.add(i);
 	}
 	for (int s=9;s<23;s++) {
