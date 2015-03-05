9810b7731b60a7bafb7cb0809ce40c3e3381e6bb
psydeolite <maryged@gmail.com>
2015-03-05 08:41:25 -0500

    the selection has begun

---
+++ 05-selection/Select.java
@@ -0,0 +1,7 @@
+public class Select {
+    private ArrayList<Integer> array=new ArrayList<Integer>();
+    public int selectHelp(int k, int start, int end) {
+    }
+    public int select(int k) {
+    }
+}
