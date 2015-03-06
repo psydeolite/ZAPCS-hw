0447ae8b883603b8d8506c1fd36f383bcb75667a
Mariya Gedrich <maryged@gmail.com>
2015-03-05 22:16:03 -0500

    the great partition

--- 05-selection/Select.java
+++ 05-selection/Select.java
@@ -1,7 +1,45 @@
+import java.util.*;
+import java.io.*;
+
 public class Select {
-    private ArrayList<Integer> array=new ArrayList<Integer>();
-    public int selectHelp(int k, int start, int end) {
+    private Random r=new Random();
+    private ArrayList<Integer> array;
+    public Select() {
+	array=new ArrayList<Integer>();
+	array.add(3);
+	array.add(6);
+	array.add(1);
+	array.add(9);
+	array.add(25);
+	array.add(5);
     }
+
+    public int selectHelp(int k, int start, int end) {
+	int ref=r.nextInt(end)+start;
+	ArrayList<Integer> newlist=new ArrayList<Integer>();
+	newlist.add(ref);
+	for (int i=0; i<end;i++) {
+	    //go through the list and do the selection/swapping
+	}
+	if (newlist.indexOf(ref)==k || start>=end) {
+	    return ref;
+	} else if (newlist.indexOf(ref)<k) {
+	    return selectHelp(k, (start+end)/2, end);
+	} else {
+	    return selectHelp(k, start, (start+end)/2);
+	}
+    }
+		       
     public int select(int k) {
+	if (k>array.size()) {
+	    System.out.println("Invalid k value");
+	    return 0; //temporary
+	}
+	return selectHelp(k, 0, array.size());
+    }
+
+    public static void main(String[] args) {
+	Select s=new Select();
+	s.select(3);
     }
 }
