988a3c71d9281209482286d698b9e53c85b0abac
Mariya Gedrich <maryged@gmail.com>
2015-03-22 21:11:21 -0400

    drunk Driver, not sure what's wrong

--- 09-iterators/LLit.java
+++ 09-iterators/LLit.java
@@ -2,22 +2,36 @@ import java.io.*;
 import java.util.*;
 
 public class LLit<E> implements Iterator<E>{
-		private Node<E> t;
-		public LLit(Node<E> n){
-				t=n;
-		}
+    private Node<E> t;
+    private Node<E> first;
+    private Node<E> lnext;
+    public LLit(Node<E> n){
+	t=n;
+	first=n;
+    }
 
-		public boolean hasNext(){
-				return t!=null;
-		}
+    public boolean hasNext(){
+	return t!=null;
+    }
 
-		public E next(){
-				E retval = t.getData();
-				t=t.getNext();
-				return retval;
-		}
+    public boolean hasNext(Node n) {
+	return n!=null;
+    }
 
-		public void remove() {
-				// fill this in later
-		}
+    public E next(){
+	E retval = t.getData();
+	lnext=t;
+	t=t.getNext();
+	return retval;
+    }
+
+    public void remove() {
+	Node one=first;
+	while (hasNext(one)) {
+	    if (one.getNext()==lnext) 
+		one.setNext(lnext.getNext());
+	    else 
+		one=one.getNext();
+	}
+    }
 }
