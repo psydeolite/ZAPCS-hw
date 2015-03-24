7ffac3884145eaa3271f3164563987a4fa1b65fa
psydeolite <maryged@gmail.com>
2015-03-24 08:37:52 -0400

    I hate arrays

---
+++ 10-stacks/ADriver.java
@@ -0,0 +1,10 @@
+public class ADriver {
+    public static void main(String[] args) {
+	myArrayStack a=new myArrayStack();
+	a.push(5);
+	System.out.println(a);
+    }
+    
+
+
+}
---
+++ 10-stacks/myArrayStack.java
@@ -0,0 +1,45 @@
+public class myArrayStack {
+    int[] list;
+    int len=0;
+    public myArrayStack() {
+	list=new int[0];
+    }
+
+    public void push(int data) {
+	len++;
+	int[] nlist=new int[len];
+	for (int i=0;i<len-1;i++) {
+	    nlist[i]=list[i];
+	}
+	nlist[len-1]=data;
+	list=nlist;
+    }
+
+    public int pop() {
+	int returnVal=list[len];
+	len--;
+	int[] nlist=new int[len];
+	for (int i=0;i<len;i++) {
+	    nlist[i]=list[i];
+	}
+	list=nlist;
+	return returnVal;
+    }
+
+    public boolean empty() {
+	return (len==0);
+    }
+
+    public int top() {
+	return list[len];
+    }
+
+    public String toString() {
+	String s="";
+	for (int i=len-1; i>0;i--) {
+	    s+=list[i]+", ";
+	}
+	return s;		  
+    }
+    
+}
