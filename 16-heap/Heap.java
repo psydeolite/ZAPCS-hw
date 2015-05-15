823dc78b4a0988e8eed82db49caa6bdbef9938ca
psydeolite <maryged@gmail.com>
2015-05-15 08:39:53 -0400

    insert/pushedDown

--- 16-heap/Heap.java
+++ 16-heap/Heap.java
@@ -1,3 +1,6 @@
+import java.util.Random;
+
+
 public class Heap {
     private int[] ar;
     private int length;
@@ -7,14 +10,50 @@ public class Heap {
     }
 
     public void insert(int n) {
-	
+	ar[length]=n;
+	length++;
+	for (int i=0;i<length;i++) {
+	    pushDown();
+	}
     }
+
+    public removeMin
 
     public void pushDown(){
 	for (int i=0; (2*i)+1<length;i++) {
 	    if (ar[i]>ar[(2*i)+1]) {
-		
+		int at2=ar[(2*i)+1];	
+		ar[(2*i)+1]=ar[i];
+		ar[i]=at2;
+	    } else if (ar[i] > ar[(2*i)]){
+		int at2 = ar[(2*i)];
+		ar[(2*i)] = ar[i];
+		ar[i]=at2;
+	    }
 	}
-
     }
+
+    public String toString(){
+	String s = "";
+	for(int i = 0; i< length;i++){
+	    s += ar[i] + " ";
+	}
+	return s;
+    }
+    
+    public static void main(String args[]){
+	Heap h = new Heap();
+	Random rnd = new Random();
+	for(int i = 0;i<20;i++){
+	    h.insert( rnd.nextInt(20) +1);
+	}
+	System.out.println(h);
+	h.insert(30);
+	h.insert(-6);
+	System.out.println(h);
+    }
+	
+		
+
 }
+
